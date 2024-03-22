3a3c58f305e5ad18cd36d544cf2f38590b442256
bibhubiswajit007 <158497554+bibhubiswajit007@users.noreply.github.com>
2024-03-22 11:00:44 +0530

    23CE01015LA7Q2.c

--- 23CE01015LA7Q2.c
+++ 23CE01015LA7Q2.c
@@ -11,22 +11,22 @@ int main(){
         if(!k) instr[i]='@';
         i++;
     }
-    int j=0,swas=0,r=strlen(instr);
-    while(swas==0){
+    int j=0,bibhu=0,r=strlen(instr);
+    while(bibhu==0){
         for (int i=0;i<r;i++){
             if(instr[i]=='@') {
                 for (int p=i;p<r;p++){
                 instr[p]=instr[p+1];
             }
             r=r-1;
-            swas=1;
+            bibhu=1;
             break;
             }
         }
     for (int p=0;p<r;p++){
         int k=((instr[p]<=90)&&(instr[p]>=65))||((instr[p]>=97&&instr[p]<=122));
         if(!k) {
-            swas=0;
+            bibhu=0;
             break;
         }
     }

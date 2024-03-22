343c030ac3dfca111facf19b6ee767aedc641406
bibhubiswajit007 <158497554+bibhubiswajit007@users.noreply.github.com>
2024-03-22 10:59:33 +0530

    23CE01015LA7Q1.c

--- 23CE01015LA7Q1.c
+++ 23CE01015LA7Q1.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 #include<string.h>
 int main(){
-    char swas[50],a;
+    char bibhu[50],a;
     printf("Enter the string :");
-    gets(swas);
+    gets(bibhu);
     printf("Enter alphabet :");
     scanf("%c",&a);
     int count=0;
-    for (int i=0;i<strlen(swas);i++){
-        if (a==swas[i]) count++;
+    for (int i=0;i<strlen(bibhu);i++){
+        if (a==bibhu[i]) count++;
     }
     printf("Frequency of %c :%d",a,count);
     return 0;
